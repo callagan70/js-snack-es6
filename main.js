e1a5ab6d0edfd98ad0cf1ac0e2bb11b526bcbb6b
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-25 16:54:41 +0000

    Work in progress...

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+var l = 0
+var pesante = 0
 const bici = [
 {
     'nombre' : 'Chester',
@@ -14,3 +17,27 @@ const bici = [
 ]
 
 console.log(bici)
+
+for (i = 0; i <= bici.length; i++){
+     l++
+    document.getElementById("bike").innerHTML +=  
+    `
+    <div>La ${l}° bicicletta si chiama: ${bici[i]['nombre']}: </div>
+    <div>Pesa: ${bici[i]['peso']} kg</div>
+    `
+    if (bici[i]['peso'] > bici[l]['peso'] ){
+        
+    } 
+}
+
+
+
+
+
+// const bici2 []
+// for ( i= 0; i <= bici.length; i++){
+// [bici[i]["nombre"], ]
+
+
+
+// }
