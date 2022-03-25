8ec7851976c1a963e73f8eb7e1a36a98523cd876
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-25 16:01:37 +0000

    Da errore....

---
+++ main.js
@@ -0,0 +1,16 @@
+const bici = [
+{
+    'nombre' = 'Chester',
+    'peso' = 5.6,
+},
+{
+    'nombre' = 'XXlite EVO',
+    'peso' = 4.3,
+},
+{
+    'nombre' = 'Marin',
+    'peso' = 3.2,
+}
+]
+
+console.log(bici)
